a39561aaa8d62b0e6377dfa5d7b63fb0970af66d
Ard <ard@protonmail.ch>
2018-07-11 21:18:44 +0100

    CI: -o StrictHostKeyChecking=no

--- swarm-push.sh
+++ swarm-push.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-scp json.key $SSH_USER@$HOST:~/json.key
-ssh $SSH_USER@$HOST 'docker login -u _json_key --password-stdin https://eu.gcr.io < ~/json.key && docker stack deploy -c docker-stack.yml --with-registry-auth $SERVICE && rm ~/json.key' 
+scp -o StrictHostKeyChecking=no json.key $SSH_USER@$HOST:~/json.key
+ssh -o StrictHostKeyChecking=no $SSH_USER@$HOST 'docker login -u _json_key --password-stdin https://eu.gcr.io < ~/json.key && docker stack deploy -c docker-stack.yml --with-registry-auth $SERVICE && rm ~/json.key' 
