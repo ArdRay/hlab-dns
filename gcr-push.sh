3f0819d16fb8a2a96df9edd2093ffbbb5d54e7f5
Ard <ard@protonmail.ch>
2018-07-11 18:48:27 +0100

    Based 64ed json key.

--- gcr-push.sh
+++ gcr-push.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-docker login -u _json_key --password-stdin https://gcr.io < json.key
+docker login -u _json_key --password-stdin https://gcr.io < $(cat json.key| base64 --decode)
 docker tag hlab-dns eu.gcr.io/axial-radius-203218/hlab-dns:x86
 docker push eu.gcr.io/axial-radius-203218/hlab-dns:x86
