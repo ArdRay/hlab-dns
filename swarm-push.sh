82743530c8880902a293b9d1918d5fe1ac41d2dc
Ard <ard@protonmail.ch>
2018-07-11 22:12:48 +0100

    CD: Debugging

--- swarm-push.sh
+++ swarm-push.sh
@@ -6,4 +6,6 @@ echo $SERVICE
 
 scp -q -o StrictHostKeyChecking=no json.key $SSH_USER@$HOST:~/json.key
 scp -q -o StrictHostKeyChecking=no docker-stack.yml $SSH_USER@$HOST:~/docker-stack.yml
-ssh -q -o StrictHostKeyChecking=no $SSH_USER@$HOST 'docker login -u _json_key --password-stdin https://eu.gcr.io < ~/json.key && docker stack deploy -c ~/docker-stack.yml $SERVICE && rm ~/json.key && rm ~/docker-stack.yml'
+ssh -q -o StrictHostKeyChecking=no $SSH_USER@$HOST 'docker login -u _json_key --password-stdin https://eu.gcr.io < ~/json.key'
+ssh -q -o StrictHostKeyChecking=no $SSH_USER@$HOST 'docker stack deploy -c ~/docker-stack.yml $SERVICE'
+ssh -q -o StrictHostKeyChecking=no $SSH_USER@$HOST 'rm ~/json.key && rm ~/docker-stack.yml'
