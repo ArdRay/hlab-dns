62cc296bd70baf016b9c232e268167bb41036549
Ard <ard@protonmail.ch>
2018-07-11 21:45:47 +0100

    CD: docker-stack.yml minor issue.

--- swarm-push.sh
+++ swarm-push.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-scp -o StrictHostKeyChecking=no json.key $SSH_USER@$HOST:~/json.key
-scp -o StrictHostKeyChecking=no docker-stack.yml $SSH_USER@$HOST:~/docker-stack.yml
-ssh -o StrictHostKeyChecking=no $SSH_USER@$HOST 'docker login -u _json_key --password-stdin https://eu.gcr.io < ~/json.key && docker stack deploy --with-registry-auth -c ~/.docker-stack.yml $SERVICE && rm ~/json.key && rm ~/docker-stack.yml'
+scp -q -o StrictHostKeyChecking=no json.key $SSH_USER@$HOST:~/json.key
+scp -q -o StrictHostKeyChecking=no docker-stack.yml $SSH_USER@$HOST:~/docker-stack.yml
+ssh -q -o StrictHostKeyChecking=no $SSH_USER@$HOST 'docker login -u _json_key --password-stdin https://eu.gcr.io < ~/json.key && docker stack deploy --with-registry-auth -c ~/.docker-stack.yml $SERVICE && rm ~/json.key && rm ~/docker-stack.yml'
