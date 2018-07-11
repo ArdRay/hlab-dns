7f722516fb7a9e032358ff852c5b90ad5c88d803
Ard <ard@protonmail.ch>
2018-07-11 22:57:59 +0100

    CD: Debugging

--- swarm-push.sh
+++ swarm-push.sh
@@ -4,5 +4,5 @@ set -e
 
 scp -q -o 'StrictHostKeyChecking=no' json.key $SSH_USER@$HOST:~/json.key
 scp -q -o 'StrictHostKeyChecking=no' docker-stack.yml $SSH_USER@$HOST:~/docker-stack.yml
-ssh -q -o 'StrictHostKeyChecking=no' $SSH_USER@$HOST 'docker stack deploy -c ~/docker-stack.yml $SERVICE && docker stack deploy --compose-file ~/docker-stack.yml dns'
+ssh -q -o 'StrictHostKeyChecking=no' $SSH_USER@$HOST 'docker login -u _json_key --password-stdin https://eu.gcr.io < ~/json.key && docker stack deploy --compose-file docker-stack.yml dns'
 ssh -q -o 'StrictHostKeyChecking=no' $SSH_USER@$HOST 'rm ~/json.key && rm ~/docker-stack.yml'
