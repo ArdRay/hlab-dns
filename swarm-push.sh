ff6253f3f8c94c13f789610289c1f16f2a635921
Ard <ard@protonmail.ch>
2018-07-11 22:19:17 +0100

    CD: Debugging

--- swarm-push.sh
+++ swarm-push.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-echo $SERVICE
-
-scp -q -o StrictHostKeyChecking=no json.key $SSH_USER@$HOST:~/json.key
-scp -q -o StrictHostKeyChecking=no docker-stack.yml $SSH_USER@$HOST:~/docker-stack.yml
-ssh -q -o StrictHostKeyChecking=no $SSH_USER@$HOST 'docker login -u _json_key --password-stdin https://eu.gcr.io < ~/json.key'
-ssh -q -o StrictHostKeyChecking=no $SSH_USER@$HOST 'docker stack deploy -c ~/docker-stack.yml $SERVICE'
-ssh -q -o StrictHostKeyChecking=no $SSH_USER@$HOST 'rm ~/json.key && rm ~/docker-stack.yml'
+scp -q -o 'StrictHostKeyChecking=no' json.key $SSH_USER@$HOST:~/json.key
+scp -q -o 'StrictHostKeyChecking=no' docker-stack.yml $SSH_USER@$HOST:~/docker-stack.yml
+ssh -q -o 'StrictHostKeyChecking=no' $SSH_USER@$HOST 'docker login -u _json_key --password-stdin https://eu.gcr.io < ~/json.key'
+ssh -q -o 'StrictHostKeyChecking=no' $SSH_USER@$HOST 'docker stack deploy -c ~/docker-stack.yml $SERVICE'
+ssh -q -o 'StrictHostKeyChecking=no' $SSH_USER@$HOST 'rm ~/json.key && rm ~/docker-stack.yml'
