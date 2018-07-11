e88edfd0edcf6e75e6ce2b3cb94057e6c56fa622
Ard <ard@protonmail.ch>
2018-07-11 21:30:40 +0100

    CD: Fixing swarm push.

--- swarm-push.sh
+++ swarm-push.sh
@@ -4,4 +4,4 @@ set -e
 
 scp -o StrictHostKeyChecking=no json.key $SSH_USER@$HOST:~/json.key
 scp -o StrictHostKeyChecking=no docker-stack.yml $SSH_USER@$HOST:~/docker-stack.yml
-ssh -o StrictHostKeyChecking=no $SSH_USER@$HOST 'docker login -u _json_key --password-stdin https://eu.gcr.io < ~/json.key && docker stack deploy -c docker-stack.yml --with-registry-auth $SERVICE && rm ~/json.key && rm ~/docker-stack.yml' 
+ssh -o StrictHostKeyChecking=no $SSH_USER@$HOST 'docker login -u _json_key --password-stdin https://eu.gcr.io < ~/json.key && docker stack deploy --with-registry-auth -c ~/.docker-stack.yml $SERVICE && rm ~/json.key && rm ~/docker-stack.yml'
