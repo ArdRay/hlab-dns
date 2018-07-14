ae1e15529350c9b156abee593258c6e3c31a4ffc
Ard <ard@protonmail.ch>
2018-07-15 00:29:37 +0100

    Multiple architectures. Part 11.

--- swarm-push.sh
+++ swarm-push.sh
@@ -4,5 +4,5 @@ set -e
 
 scp -q -o 'StrictHostKeyChecking=no' json.key $SSH_USER@$HOST:~/json.key
 scp -q -o 'StrictHostKeyChecking=no' docker-stack.yml $SSH_USER@$HOST:~/docker-stack.yml
-ssh -q -o 'StrictHostKeyChecking=no' $SSH_USER@$HOST 'docker login -u _json_key --password-stdin https://eu.gcr.io < ~/json.key && docker stack deploy --registry-auth --compose-file docker-stack.yml dns'
+ssh -q -o 'StrictHostKeyChecking=no' $SSH_USER@$HOST 'docker login -u _json_key --password-stdin https://eu.gcr.io < ~/json.key && docker stack deploy --with-registry-auth --compose-file docker-stack.yml dns'
 ssh -q -o 'StrictHostKeyChecking=no' $SSH_USER@$HOST 'rm ~/json.key && rm ~/docker-stack.yml'
