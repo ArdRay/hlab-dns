edfa1532fc5fa59f21268dfb69ff2134a7be3bcf
Ard <ard@protonmail.ch>
2018-07-14 23:13:06 +0100

    Multiple architectures. Part 2.

--- gcr-push.sh
+++ gcr-push.sh
@@ -3,5 +3,8 @@
 set -e
 
 docker login -u _json_key --password-stdin https://eu.gcr.io < json.key
-docker tag hlab-dns eu.gcr.io/$PROJECT_ID/hlab-dns:x86
-docker push eu.gcr.io/$PROJECT_ID/hlab-dns:x86
+docker tag hlab-dns:amd64 eu.gcr.io/$PROJECT_ID/hlab-dns:amd64
+docker push eu.gcr.io/$PROJECT_ID/hlab-dns:amd64
+
+docker tag hlab-dns:arm6 eu.gcr.io/$PROJECT_ID/hlab-dns:arm6
+docker push eu.gcr.io/$PROJECT_ID/hlab-dns:arm6
